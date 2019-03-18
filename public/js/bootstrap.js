7dcfbec0aa4a6bd08b892c445a4263e1b0a5cfaa
dyh-sjtu <dyh_sjtu@163.com>
2019-03-18 15:33:03 +0800

    fix

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -55,6 +55,165 @@
 	
 	/**
 	 * --------------------------------------------------------------------------
+	 * Bootstrap (v4.0.0): util.js
+	 * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+	 * --------------------------------------------------------------------------
+	 */
+	
+	var Util = function ($$$1) {
+		/**
+		 * ------------------------------------------------------------------------
+		 * Private TransitionEnd Helpers
+		 * ------------------------------------------------------------------------
+		 */
+		var transition = false;
+		var MAX_UID = 1000000; // Shoutout AngusCroll (https://goo.gl/pxwQGp)
+		
+		function toType(obj) {
+			return {}.toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+		}
+		
+		function getSpecialTransitionEndEvent() {
+			return {
+				bindType: transition.end,
+				delegateType: transition.end,
+				handle: function handle(event) {
+					if ($$$1(event.target).is(this)) {
+						return event.handleObj.handler.apply(this, arguments); // eslint-disable-line prefer-rest-params
+					}
+					
+					return undefined; // eslint-disable-line no-undefined
+				}
+			};
+		}
+		
+		function transitionEndTest() {
+			if (typeof window !== 'undefined' && window.QUnit) {
+				return false;
+			}
+			
+			return {
+				end: 'transitionend'
+			};
+		}
+		
+		function transitionEndEmulator(duration) {
+			var _this = this;
+			
+			var called = false;
+			$$$1(this).one(Util.TRANSITION_END, function () {
+				called = true;
+			});
+			setTimeout(function () {
+				if (!called) {
+					Util.triggerTransitionEnd(_this);
+				}
+			}, duration);
+			return this;
+		}
+		
+		function setTransitionEndSupport() {
+			transition = transitionEndTest();
+			$$$1.fn.emulateTransitionEnd = transitionEndEmulator;
+			
+			if (Util.supportsTransitionEnd()) {
+				$$$1.event.special[Util.TRANSITION_END] = getSpecialTransitionEndEvent();
+			}
+		}
+		
+		function escapeId(selector) {
+			// We escape IDs in case of special selectors (selector = '#myId:something')
+			// $.escapeSelector does not exist in jQuery < 3
+			selector = typeof $$$1.escapeSelector === 'function' ? $$$1.escapeSelector(selector).substr(1) : selector.replace(/(:|\.|\[|\]|,|=|@)/g, '\\$1');
+			return selector;
+		}
+		
+		/**
+		 * --------------------------------------------------------------------------
+		 * Public Util Api
+		 * --------------------------------------------------------------------------
+		 */
+		
+		
+		var Util = {
+			TRANSITION_END: 'bsTransitionEnd',
+			getUID: function getUID(prefix) {
+				do {
+					// eslint-disable-next-line no-bitwise
+					prefix += ~~(Math.random() * MAX_UID); // "~~" acts like a faster Math.floor() here
+				} while (document.getElementById(prefix));
+				
+				return prefix;
+			},
+			getSelectorFromElement: function getSelectorFromElement(element) {
+				var selector = element.getAttribute('data-target');
+				
+				if (!selector || selector === '#') {
+					selector = element.getAttribute('href') || '';
+				} // If it's an ID
+				
+				
+				if (selector.charAt(0) === '#') {
+					selector = escapeId(selector);
+				}
+				
+				try {
+					var $selector = $$$1(document).find(selector);
+					return $selector.length > 0 ? selector : null;
+				} catch (err) {
+					return null;
+				}
+			},
+			reflow: function reflow(element) {
+				return element.offsetHeight;
+			},
+			triggerTransitionEnd: function triggerTransitionEnd(element) {
+				$$$1(element).trigger(transition.end);
+			},
+			supportsTransitionEnd: function supportsTransitionEnd() {
+				return Boolean(transition);
+			},
+			isElement: function isElement(obj) {
+				return (obj[0] || obj).nodeType;
+			},
+			typeCheckConfig: function typeCheckConfig(componentName, config, configTypes) {
+				for (var property in configTypes) {
+					if (Object.prototype.hasOwnProperty.call(configTypes, property)) {
+						var expectedTypes = configTypes[property];
+						var value = config[property];
+						var valueType = value && Util.isElement(value) ? 'element' : toType(value);
+						
+						if (!new RegExp(expectedTypes).test(valueType)) {
+							throw new Error(componentName.toUpperCase() + ": " + ("Option \"" + property + "\" provided type \"" + valueType + "\" ") + ("but expected type \"" + expectedTypes + "\"."));
+						}
+					}
+				}
+			}
+		};
+		setTransitionEndSupport();
+		return Util;
+	}($);
+	
+	/**
+	 * --------------------------------------------------------------------------
+	 * Bootstrap (v4.0.0): alert.js
+	 * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+	 * --------------------------------------------------------------------------
+	 */
+	
+	
+	
+	/**
+	 * --------------------------------------------------------------------------
+	 * Bootstrap (v4.0.0): button.js
+	 * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+	 * --------------------------------------------------------------------------
+	 */
+	
+	
+	
+	/**
+	 * --------------------------------------------------------------------------
 	 * Bootstrap (v4.0.0): carousel.js
 	 * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
 	 * --------------------------------------------------------------------------
@@ -125,7 +284,7 @@
 			ITEM: '.changqiu-carousel-item',
 			NEXT_PREV: '.changqiu-carousel-item-next, .changqiu-carousel-item-prev',
 			INDICATORS: '.changqiu-carousel-indicators',
-			DATA_SLIDE: '[data-slide-changqiu]',
+			DATA_SLIDE: '[data-slide-changqiu], [data-slide-changqiuIndex]',
 			DATA_RIDE: '[data-ride="changqiu-carousel"]'
 			/**
 			 * ------------------------------------------------------------------------
@@ -492,7 +651,7 @@
 					}
 					
 					var config = _extends({}, $$$1(target).data(), $$$1(this).data());
-					var slideIndex = this.getAttribute('data-slide-changqiu');
+					var slideIndex = this.getAttribute('data-slide');
 					
 					if (slideIndex) {
 						config.interval = false;
@@ -552,8 +711,6 @@
 		return Carousel;
 	}($);
 	
-	
-	
 	/**
 	 * --------------------------------------------------------------------------
 	 * Bootstrap (v4.0.0-alpha.6): index.js
@@ -578,7 +735,17 @@
 		}
 	})($);
 	
+	// exports.Util = Util;
+	// exports.Alert = Alert;
+	// exports.Button = Button;
 	exports.Carousel = Carousel;
+	exports.Collapse = Collapse;
+	exports.Dropdown = Dropdown;
+	exports.Modal = Modal;
+	exports.Popover = Popover;
+	exports.Scrollspy = ScrollSpy;
+	exports.Tab = Tab;
+	exports.Tooltip = Tooltip;
 	
 	Object.defineProperty(exports, '__esModule', {value: true});
 	
